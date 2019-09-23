c05a924d5377e8bb555c665f97c1024152be5dd2
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2019-09-23 15:25:15 +0300

    Use a async/await instead of promise

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const countryCode = getLocation().then((location) => {
-    console.log(location);
-    return getCountry(location.country);
-}).then(country => {
+const countryName = CurrentCountry().then(country => {
     console.log(country.nativeName);
 }).catch((err) => {
     console.log(err)
 });
+
+
+// const country = getLocation().then((location) => {
+//     console.log(location);
+//     return getCountry(location.country);
+// }).then(country => {
+//     console.log(country.nativeName);
+// }).catch((err) => {
+//     console.log(err)
+// });
--- getLocation.js
+++ getLocation.js
@@ -1,25 +1,27 @@
-const getLocation = () => {
-    return fetch('http://ipinfo.io/json?token=b26071b0b24b53', {}).then(res => {
-        if(res.status === 200 ){
-            return res.json();
-        } else {
-            throw new Error('Unable to fetch data!!!');
-        }
-    }).then(location => {
-        return location;
-    })
+const getLocation = async () => {
+    const res = await fetch('http://ipinfo.io/json?token=b26071b0b24b53', {});
+    if (res.status === 200) {
+        const data = await res.json();
+        return data;
+    } else {
+        throw new Error('Unable to fetch data!!!');
+    }
 }
 
-const getCountry = (countryCode) => {
-    return fetch('https://restcountries.eu/rest/v2/all', {}).then(res => {
-        if (res.status === 200) {
-            return res.json();
-        } else {
-            throw new Error('Unable to fetch data!!!');
-        }
-    }).then(data => {
+const getCountry = async (countryCode) => {
+    const res = await fetch('https://restcountries.eu/rest/v2/all', {});
+    if (res.status === 200) {
+        const data = await res.json();
         return data.find(el => {
             return el.alpha2Code === countryCode;
         });
-    })
+    } else {
+        throw new Error('Unable to fetch data!!!');
+    }
+}
+
+
+const CurrentCountry = async () => {
+    const location = await getLocation();
+    return await getCountry(location.country);
 }
